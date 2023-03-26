87e6832f445be6f94b79443261793f61b7d9e0d7
Rishikesh Vishwakarma <rishikeshv9693@gmail.com>
2023-03-26 23:14:49 +0530

    add my graph

---
+++ part2/graph_processing/my_graph.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type MyGraph struct {
+	edges        map[int][]int
+	vertices     map[int]struct{}
+	noOfVertices int
+	noOfEdges    int
+}
+
+func NewMyGraph() *MyGraph {
+	return &MyGraph{
+		edges:        make(map[int][]int, 0),
+		vertices:     make(map[int]struct{}, 0),
+		noOfVertices: 0,
+		noOfEdges:    0,
+	}
+}
+
+func (g *MyGraph) AddEdge(v1, v2 int) {
+	g.edges[v1] = append(g.edges[v1], v2)
+	g.edges[v2] = append(g.edges[v2], v1)
+	if _, ok := g.vertices[v1]; !ok {
+		g.noOfVertices++
+	}
+	if _, ok := g.vertices[v2]; !ok {
+		g.noOfVertices++
+	}
+	g.vertices[v1] = struct{}{}
+	g.vertices[v2] = struct{}{}
+	g.noOfEdges++
+}
+
+func (g *MyGraph) Edges(v int) ([]int, bool) {
+	edges, ok := g.edges[v]
+	return edges, ok
+}
+
+func (g *MyGraph) String() string {
+	var s strings.Builder
+
+	for v := range g.vertices {
+		s.WriteString(fmt.Sprintf("%v: ", v))
+		edges, ok := g.Edges(v)
+		if ok {
+			for _, e := range edges {
+				s.WriteString(fmt.Sprintf("%v ", e))
+			}
+		}
+		s.WriteString("\n")
+	}
+
+	return s.String()
+}
